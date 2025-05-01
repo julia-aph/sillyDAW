5ffcefc886dbc94d43cf28d7ac97e67dc03c91f3
Julia <145168563+julia-aph@users.noreply.github.com>
2025-05-01 17:14:40 -0500

    Working in the car
    trying and failing to think while my battery drains to 0%

--- src/plugin_interface.zig
+++ src/plugin_interface.zig
@@ -8,20 +8,13 @@ pub const GuestMetadata = extern struct {
 
 pub fn GuestVTable(T: *type) type {
     return extern struct {
-        load: *const fn () callconv(.C) void,
-        unload: *const fn () callconv(.C) void,
-        init: *const fn () callconv(.C) ?*anyopaque,
-        deinit: *const fn (this: T) callconv(.C) void,
-        
-        processAudio: *const fn (this: T, )
+        load: *const fn (format: *const Format) callconv(.c) void,
+        unload: *const fn () callconv(.c) void,
+        changeFormat: *const fn () callconv(.c) void,
 
-        dispatchEvent: *const fn (this: T, event: *const Event) callconv(.C) bool,
-        renderVoice: *const fn (
-            this: T,
-            voice: u8,
-            stream: [*]f32,
-            frames: u32,
-        ) callconv(.C) bool,
+        init: *const fn () callconv(.c) ?*anyopaque,
+        deinit: *const fn (this: T) callconv(.c) void,
+        render: *const fn (zzxz
     };
 }
 
@@ -37,7 +30,7 @@ pub fn CustomGuest(T: *type) type {
     };
 }
 
-pub const Spec = extern struct {
+pub const Format = extern struct {
     freq: u32,
     channels: u8,
 };
@@ -46,7 +39,7 @@ pub const EventType = enum(u8) { on, off, slide, impulse };
 
 pub const Event = extern struct {
     time: u16,
-    voice: u8,
+    channel: u8,
     type: EventType,
 
     freq: f32,
