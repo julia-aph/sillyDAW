53c5bfed171485ea77e3ec139ab667a7d7a61faa
Julia <145168563+julia-aph@users.noreply.github.com>
2025-05-01 23:12:24 -0500

    Rearchitecting plugin system
    Plugins have a constant amount of audio and event io groups. Groups can have a variable number of channels during runtime.

--- src/plugin_interface.zig
+++ src/plugin_interface.zig
@@ -1,35 +1,5 @@
 const std = @import("std");
 
-pub const GuestMetadata = extern struct {
-    name: [*:0]const u8,
-    author: [*:0]const u8,
-    version: [*:0]const u8,
-};
-
-pub fn GuestVTable(T: *type) type {
-    return extern struct {
-        load: *const fn (format: *const Format) callconv(.c) void,
-        unload: *const fn () callconv(.c) void,
-        changeFormat: *const fn () callconv(.c) void,
-
-        init: *const fn () callconv(.c) ?*anyopaque,
-        deinit: *const fn (this: T) callconv(.c) void,
-        render: *const fn (zzxz
-    };
-}
-
-pub const Guest = extern struct {
-    meta: GuestMetadata,
-    vt: GuestVTable(*anyopaque),
-};
-
-pub fn CustomGuest(T: *type) type {
-    return extern struct {
-        meta: GuestMetadata,
-        vt: GuestVTable(T),
-    };
-}
-
 pub const Format = extern struct {
     freq: u32,
     channels: u8,
@@ -46,3 +16,54 @@ pub const Event = extern struct {
     velocity: u8,
     pan: i8,
 };
+
+pub const EventGroup = extern struct {
+    ptr: [*]Event,
+    count: u8,
+};
+
+pub const AudioGroup = extern struct {
+    ptr: [*][*]f32,
+    count: u8,
+};
+
+pub const GuestMetadata = extern struct {
+    name: [*:0]const u8,
+    author: [*:0]const u8,
+    version: [*:0]const u8,
+
+    event_inputs: u8,
+    audio_inputs: u8,
+    event_outputs: u8,
+    audio_outputs: u8,
+};
+
+pub fn GuestVTable(P: *type) type {
+    return extern struct {
+        load: *const fn (format: *const Format) callconv(.x86_64_sysv) void,
+        unload: *const fn () callconv(.x86_64_sysv) void,
+        changeFormat: *const fn () callconv(.x86_64_sysv) void,
+
+        init: *const fn () callconv(.x86_64_sysv) ?*anyopaque,
+        deinit: *const fn (this: P) callconv(.x86_64_sysv) void,
+        process: *const fn (
+            this: P,
+            event_in: [*]const EventGroup,
+            audio_in: [*]const AudioGroup,
+            event_out: [*]EventGroup,
+            audio_out: [*]AudioGroup,
+        ) callconv(.x86_64_sysv) void,
+    };
+}
+
+pub const Guest = extern struct {
+    meta: GuestMetadata,
+    vt: GuestVTable(*anyopaque),
+};
+
+pub fn CustomGuest(T: *type) type {
+    return extern struct {
+        meta: GuestMetadata,
+        vt: GuestVTable(T),
+    };
+}
--- src/sine_plugin.zig
+++ src/sine_plugin.zig
@@ -6,10 +6,17 @@ export const implementation: plugin_interface.CustomGuest(*SinePlugin) = .{
         .name = "Sine",
         .author = "Julia",
         .version = "0.0.0",
+
+        .event_inputs = 0,
+        .audio_inputs = 0,
+        .event_outputs = 0,
+        .audio_outputs = 0,
     },
     .vt = .{
         .load = &SinePlugin.load,
         .unload = &SinePlugin.unload,
+        .changeFormat = &SinePlugin.changeFormat,
+
         .init = &SinePlugin.init,
         .deinit = &SinePlugin.deinit,
         .dispatchEvent = &SinePlugin.dispatchEvent,
@@ -20,23 +27,25 @@ export const implementation: plugin_interface.CustomGuest(*SinePlugin) = .{
 const SinePlugin = struct {
     var format: *const plugin_interface.Format;
 
-    fn load(format: *const plugin_interface.Format) callconv(.C) void {
+    fn load(format: *const plugin_interface.Format) callconv(.x86_64_sysv) void {
         SinePlugin.format = format;
     }
 
-    fn unload() callconv(.C) void {}
+    fn unload() callconv(.x86_64_sysv) void {}
 
-    fn changeFormat() callconv(.C) void {}
+    fn changeFormat() callconv(.x86_64_sysv) void {}
 };
 
 const SineInstance = struct {
-
-
-    fn init() callconv(.C) ?*SinePlugin {
+    fn init() callconv(.x86_64_sysv) ?*SinePlugin {
         return std.heap.c_allocator.create(SinePlugin) catch null;
     }
 
-    fn deinit(sine: *SinePlugin) callconv(.C) void {
+    fn deinit(sine: *SinePlugin) callconv(.x86_64_sysv) void {
         std.heap.c_allocator.destroy(sine);
+    }
+
+    fn process(sine: *SinePlugin, event_in: [*]const plugin_interface.EventGroup, [*]const plugin_interface.AudioGroup, event_out: [*]plugin_interface.EventGroup, audio_out: [*]plugin_interface.AudioGroup,) callconv(.x86_64_sysv) void {
+
     }
 }
